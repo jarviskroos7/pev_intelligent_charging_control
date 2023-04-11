ee75a3fa3351be8860aa57dec5cbe50f302ce464
jarvis <jarviskroos7@berkeley.edu>
2023-04-11 02:41:30 -0700

    Update utils.py

--- rl/simple_charge_example/utils.py
+++ rl/simple_charge_example/utils.py
@@ -1,3 +1,16 @@
 import numpy as np
 import pandas as pd
 
+def get_price(current_time, day=None):
+
+    if day:
+        price = pd.read_csv(f'../data/price_day_{day}_idx.csv')['price'].values
+    else:
+        price = pd.read_csv('../data/price_day_idx.csv')['price'].values
+    
+    try:
+        return price[current_time]
+    except Exception as e:
+        print('incorrect time index:', e)
+        return None
+
