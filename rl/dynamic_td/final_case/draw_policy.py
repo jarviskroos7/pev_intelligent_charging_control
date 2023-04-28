f37b3e066bc18e289d0bd103790e5956393875fe
Jarvis Yuan <jarviskroos7@berkeley.edu>
2023-04-28 11:08:47 -0700

    updated draw_policy.py

--- rl/dynamic_td/final_case/draw_policy.py
+++ rl/dynamic_td/final_case/draw_policy.py
@@ -10,36 +10,64 @@ state = [0.8, 119, 0]
 done = False
 charge_env = final_env()
 
-action_history = [None] * charge_env.state_size_time
-reward_history = [None] * charge_env.state_size_time
+baseline_action = [None] * charge_env.state_size_time 
+opt_action = baseline_action.copy()
+baseline_reward = baseline_action.copy()
+opt_reward = baseline_action.copy()
 
+# step through optimal policy
 while not done:
     state_0_index = charge_env.get_index(state[0])
     action_prob = policy[state_0_index, state[1], state[2], :]
     action = np.argmax(action_prob)
-    # if action !=0:
-    action_history[state[2]] = action
+    opt_action[state[2]] = action
     new_state, reward, done = charge_env.step(state, action)
-    reward_history[state[2]] = reward
+    opt_reward[state[2]] = reward
     state = new_state
 
-for idx, action in enumerate(action_history):
+# step through baseline
+state = [0.8, 119, 0]
+done = False
+
+while not done:
+    action = 2
+    baseline_action[state[2]] = action
+    new_state, reward, done = charge_env.step(state, action)
+    baseline_reward[state[2]] = reward
+    state = new_state
+
+for idx, action in enumerate(opt_action):
     if action == 0:
         # discharging
-        action_history[idx] = -1
+        opt_action[idx] = -1
     elif action == 1:
         # do nothing
-        action_history[idx] = 0
+        opt_action[idx] = 0
     elif action == 2:
         # charging
-        action_history[idx] = 1
+        opt_action[idx] = 1
     else:
         pass
 
-print(action_history)
-action_history = [0 if a is None else a for a in action_history]
+baseline_action = [0 if a is None else a for a in baseline_action]
+baseline_action = np.array(baseline_action) / 2
 
-print("Total SOC charged =", sum(action_history) * charge_env.delta_soc_interval)
-print("Total state value =", sum(filter(None, reward_history)))
-print("Total energyCharged cost = $", np.array(action_history) @ np.array(charge_env.price_curve) \
-      * charge_env.delta_soc_interval)
+print(opt_action)
+opt_action = [0 if a is None else a for a in opt_action]
+opt_cost = np.array(opt_action) @ np.array(charge_env.price_curve) \
+      * charge_env.delta_soc_interval
+baseline_cost = np.array(baseline_action) @ np.array(charge_env.price_curve) \
+      * charge_env.delta_soc_interval
+
+print("======== Optimal Policy: ========")
+print("Total SOC charged =", sum(opt_action) * charge_env.delta_soc_interval)
+print("Total state value =", sum(filter(None, opt_reward)))
+print("Total energyCharged cost = $", opt_cost)
+
+print()
+print("======= Baseline Policy: ========")
+print("Total energyCharged cost = $", baseline_cost)
+
+print()
+print("========= Evaluation : ==========")
+print("Charging Cost saved = $", round(opt_cost - baseline_cost, 3), ",", round((opt_cost - baseline_cost) / baseline_cost * 100, 3), "%")
